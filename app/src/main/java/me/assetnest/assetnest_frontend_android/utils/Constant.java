7e8fba858303d654341b989a36e24ba809582b40
iqbal nur imansyah <iqbalnurimansyah@gmail.com>
2020-12-23 19:08:58 +0700

    ganti ke https

--- app/src/main/java/me/assetnest/assetnest_frontend_android/utils/Constant.java
+++ app/src/main/java/me/assetnest/assetnest_frontend_android/utils/Constant.java
@@ -1,6 +1,6 @@
 package me.assetnest.assetnest_frontend_android.utils;
 
 public class Constant {
-    public static final String  BASE_URL = "http://api.assetnest.me/";
+    public static final String  BASE_URL = "https://api.assetnest.me/";
     public static final String  SHOW_ASSET = BASE_URL + "assets/";
 }
